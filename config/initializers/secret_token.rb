80a6dd4028995d19d1598f034936db58490a254a
Presha Rajbhandary <presharajbhandary@yahoo.com>
2011-12-02 12:09:00 +0545

    Tour description added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tour::Application.config.secret_token = '87c4cd4c3b5b73924abe7bfe8ff98333e94f78184712b348d705f55b538e93ce47162496a73998285b781196f22c516a625ce521cded9ffdfa8aa2f87e38590a'
